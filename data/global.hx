a7a94b9c95a1ac522948581bc7b6bdcf51594fa0
playeah2 <playeah50@gmail.com>
2024-02-17 17:51:17 +0100

    test

--- data/global.hx
+++ data/global.hx
@@ -1,6 +1,12 @@
 window.title =  "FNF : CLASSIFIED";
 
+var oldtv = new CustomShader('oldTVShader');
+//var tv = new CustomShader('wave');
+
 function update(elapsed){
     if (FlxG.keys.justPressed.F5)
         FlxG.resetState();
+}
+function postCreate(){
+    camGame.addShader(oldtv);
 }
